cb864a0eeb44735edd2b905f7cace8e57d2162d2
acostadd <acostarojasas@gmail.com>
2025-03-04 18:06:00 -0600

    errores_:(

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.core:core-ktx:1.7.0")
+    implementation(libs.androidx.activity)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     implementation ("androidx.appcompat:appcompat:1.4.0") // Versión más reciente de appcompat
--- app/src/main/java/com/example/proyect/MainActivity.kt
+++ app/src/main/java/com/example/proyect/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.proyect
 
+
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -11,21 +16,15 @@ import com.example.proyect.databinding.ActivityMainBinding
 
 
 
-
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -36,5 +35,31 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Encuentra los botones (TextViews) y configura los listeners
+        val noticias = findViewById<TextView>(R.id.noticias)
+        noticias.setOnClickListener {
+            goToNoticias() // Navegar a la actividad de noticias
+        }
+
+        val horario = findViewById<TextView>(R.id.horario)
+        horario.setOnClickListener {
+            goToHorario() // Navegar a la actividad de horario
+        }
+    }
+
+    private fun goToNoticias() {
+        // Crear el intent para ir a MainActivity2 (o la actividad de noticias)
+        val i = Intent(this, MainActivity2::class.java)
+        startActivity(i)
+    }
+
+    private fun goToHorario() {
+        // Crear el intent para ir a MainActivity3 (o la actividad de horario)
+        val i = Intent(this, MainActivity3::class.java)
+        startActivity(i)
     }
 }
+
+
+
